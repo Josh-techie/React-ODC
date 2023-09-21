2cfca60219642c7016a59841d1739dfa2cab86d2
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-21 15:27:02 +0100

    Counter w useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 
-class MountingExample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { message: 'Hello' };
-  }
+function Counter() {
+  // Initialize a state variable 'count' with an initial value of 0
+  const [count, setCount] = useState(0);
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ message: 'Hello, React!' });
-    }, 2000);
-  }
+  // Function to increment the count
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-  render() {
-    return <div>{this.state.message}</div>;
-  }
+  // Function to decrement the count
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div class="bg-slate-500">
+      <h1>  <b> Counter App </b> </h1>
+      <p>Count: {count}</p>
+      <button  onClick={increment}>Increment</button><br></br>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
-export default MountingExample;
+export default Counter;
