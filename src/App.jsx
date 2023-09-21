4026d7fed020d2f4de5d21670726a71b03874f35
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-21 12:35:29 +0100

    Rendering Componant

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-//Compostant enfant qui affiche ine liste des tâches
-class TaskList extends Component {
-  render (){
-   const {tasks, onDeleteTask} = this.props; 
-  return (
-    <ul>
-      {
-      tasks.map((task, index) => 
-      (<li key={index}>
-        {task}
-        <button class="ml-3 bg-red-100" onClick={()=>onDeleteTask(index)}>Supprimer</button>
-      </li>
-      ))}
-    </ul>);
-  }
-}
-
-//Compasant parent qui gére liste de tâche
-class App extends Component {
-  constructor (props){
+class MountingExample extends Component {
+  constructor(props) {
     super(props);
-    this.state = {
-      tasks: ['Tâche 1','Tâche 2','Tâche 3','Tâche 4', 'Tâche 5'],
-    };
+    this.state = { message: 'Hello' };
   }
-  
- 
-  handleDeleteTask = (index) => {
-    // Supprimer une tâche de la liste
-    const updatedTasks = this.state.tasks.slice();
-    updatedTasks.splice(index, 1);
-    this.setState({ tasks: updatedTasks }); // Corrected the property name to 'tasks'
-  };
 
-  render (){
-    return (
-      <div class="ml-4">
-        <br></br>
-        <h2> <b> Liste de Tâches</b></h2><br></br>
-        <TaskList tasks={this.state.tasks} onDeleteTask={this.handleDeleteTask} />
-      </div>
-    );
-  }   
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ message: 'Hello, React!' });
+    }, 2000);
+  }
+
+  render() {
+    return <div>{this.state.message}</div>;
+  }
 }
 
-export default App
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export default MountingExample;
