74f4edaa5cd5375fc6af1bb957bc71bc179a9b02
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-21 11:18:54 +0100

    Run-App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  
+  return (
+    <>
+
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dignissimos corrupti? Maxime quod, illum esse incidunt provident officiis odit ullam facere, doloribus aliquid ipsa ab voluptates maiores recusandae reprehenderit cumque.</p>
+
+    </>
+  )
+}       
+
+export default App
