25c89aaa2a27dac212361a6a3fe446c12bbd1035
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-21 12:13:41 +0100

    To Do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-
-
-  
+//Compostant enfant qui affiche ine liste des tâches
+class TaskList extends Component {
+  render (){
+   const {tasks, onDeleteTask} = this.props; 
   return (
-    <>
+    <ul>
+      {
+      tasks.map((task, index) => 
+      (<li key={index}>
+        {task}
+        <button class="ml-3 bg-red-100" onClick={()=>onDeleteTask(index)}>Supprimer</button>
+      </li>
+      ))}
+    </ul>);
+  }
+}
 
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dignissimos corrupti? Maxime quod, illum esse incidunt provident officiis odit ullam facere, doloribus aliquid ipsa ab voluptates maiores recusandae reprehenderit cumque.</p>
+//Compasant parent qui gére liste de tâche
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      tasks: ['Tâche 1','Tâche 2','Tâche 3','Tâche 4', 'Tâche 5'],
+    };
+  }
+  
+ 
+  handleDeleteTask = (index) => {
+    // Supprimer une tâche de la liste
+    const updatedTasks = this.state.tasks.slice();
+    updatedTasks.splice(index, 1);
+    this.setState({ tasks: updatedTasks }); // Corrected the property name to 'tasks'
+  };
 
-    </>
-  )
-}       
+  render (){
+    return (
+      <div class="ml-4">
+        <br></br>
+        <h2> <b> Liste de Tâches</b></h2><br></br>
+        <TaskList tasks={this.state.tasks} onDeleteTask={this.handleDeleteTask} />
+      </div>
+    );
+  }   
+}
 
 export default App
+
+ReactDOM.render(<App />, document.getElementById('root'));
