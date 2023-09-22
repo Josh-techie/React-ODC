17b42ebc4fa4c863fcdf94947884af4058f6fd8f
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-22 10:39:03 +0100

    XML format tested

---
+++ Dingdong.jsx
@@ -0,0 +1,9 @@
+const Ding = () => {
+    return (
+        <header>
+            <h1> My Application </h1>
+        </header>
+    )
+}
+
+export default Ding
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
+import Header from '../Dingdong' 
 import React, { useState } from 'react';
 
-function Counter() {
-  // Initialize a state variable 'count' with an initial value of 0
-  const [count, setCount] = useState(0);
 
-  // Function to increment the count
-  const increment = () => {
-    setCount(count + 1);
-  };
 
-  // Function to decrement the count
-  const decrement = () => {
-    setCount(count - 1);
-  };
+function App() {
+  
+  const name = 'Brad'
 
   return (
-    <div class="bg-slate-500">
-      <h1>  <b> Counter App </b> </h1>
-      <p>Count: {count}</p>
-      <button  onClick={increment}>Increment</button><br></br>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  );
-}
 
-export default Counter;
+<div className='container'>
+    <Header />
+<p> Hello {name} from React </p>
+
+</div>
+
+    )
+  };
+
+
+export default App;
